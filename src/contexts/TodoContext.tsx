a0fdad1b5a3f9136268e87099fa3353ef871e0e2
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-02-09 02:01:39 -0300

    feat: create filter function

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -19,11 +19,13 @@ export function TodoProvider({ children }: { children: React.ReactNode }) {
   const loadTodos = useCallback(async () => {
     try {
       const result = await todosService.getAll();
-      setTodos(result);
+      const filteredTodos = applyFilter(result, filter);
+
+      setTodos(filteredTodos);
     } catch (error) {
       alert("deu ruim parceiro!!");
     }
-  }, []);
+  }, [filter]);
 
   function handleAddItem(data: ITodo) {
     setTodos([data, ...todos]);
@@ -32,6 +34,17 @@ export function TodoProvider({ children }: { children: React.ReactNode }) {
   const deleteAllTodos = useCallback(() => {
     setTodos([]);
   }, []);
+
+  const applyFilter = (todos: ITodo[], filter: string | null): ITodo[] => {
+    switch (filter) {
+      case "active":
+        return todos.filter((todo) => !todo.isDone);
+      case "completed":
+        return todos.filter((todo) => todo.isDone);
+      default:
+        return todos;
+    }
+  };
 
   useEffect(() => {
     loadTodos();
