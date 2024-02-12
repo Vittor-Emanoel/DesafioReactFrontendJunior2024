cfea688d4264f9a79b550a92b3f888d45bc4f9d0
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-02-12 10:28:42 -0300

    feat: update readme

--- src/components/TaskItem/styles.ts
+++ src/components/TaskItem/styles.ts
@@ -6,26 +6,10 @@ interface TextProps {
 
 export const Container = styled.div`
   width: 100%;
-
   display: flex;
   align-items: center;
   justify-content: space-between;
   position: relative;
-
-  input {
-    font-size: 24px;
-    padding: 15px 80px 15px 60px;
-    outline: none;
-    border: none;
-    background: #fff;
-    flex: 1;
-    cursor: pointer;
-    border: 1px solid ${(props) => props.theme["border"]};
-
-    &::placeholder {
-      color: rgba(0, 0, 0, 0.4);
-    }
-  }
 
   div {
     position: absolute;
@@ -60,4 +44,16 @@ export const Text = styled.input<TextProps>`
   text-decoration: ${(props) => (props.checked ? "line-through" : "none")};
   color: ${(props) =>
     props.checked ? props.theme["completed"] : props.theme["gray-500"]};
+  font-size: 24px;
+  padding: 15px 80px 15px 60px;
+  outline: none;
+  border: none;
+  background: #fff;
+  flex: 1;
+  cursor: pointer;
+  border: 1px solid ${(props) => props.theme["border"]};
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.4);
+  }
 `;
