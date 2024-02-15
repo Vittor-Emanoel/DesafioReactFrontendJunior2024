ac292cea8321e6dbfb41c79f2165de53e959a40d
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-02-15 00:54:15 -0300

    feat: create presentation div

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,27 @@
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Tasks } from "../../components/Tasks";
-import { Container } from "./styles";
+import { Container, PresentationContainer } from "./styles";
 
 export function Home() {
   return (
-    <Container>
-      <Header />
-      <Tasks />
-      <Footer />
-    </Container>
+    <div>
+      <Container>
+        <Header />
+        <Tasks />
+        <Footer />
+      </Container>
+
+      <PresentationContainer>
+        <small> Desenvolvido com ❤️ por </small>
+        <a
+          href="https://github.com/vittor-emanoel"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @Vittor
+        </a>
+      </PresentationContainer>
+    </div>
   );
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -9,3 +9,19 @@ export const Container = styled.section`
 
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
 `;
+
+export const PresentationContainer = styled.div`
+  text-align: center;
+  color: ${({ theme }) => theme["gray-500"]};
+  padding: 32px;
+
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.red};
+    font-weight: 500;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
