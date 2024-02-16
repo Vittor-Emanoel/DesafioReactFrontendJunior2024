599b2b5b50dca4d51fc3faede0f2cbe06376a3b1
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-02-16 14:37:22 -0300

    test: ensure that the input box will not be on screen in edit mode

--- src/components/TaskItem/taskitem.spec.tsx
+++ src/components/TaskItem/taskitem.spec.tsx
@@ -1,8 +1,15 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { TaskItem } from ".";
 import { Checkbox } from "../CheckBox";
 
 describe("Task Item component", () => {
+  const mockTask = {
+    id: "1",
+    title: "Test Task",
+    isDone: false,
+  };
+
   it("should mark the input box with a check if the checked option is true", () => {
     const handleCheckedMock = jest.fn();
 
@@ -19,5 +26,15 @@ describe("Task Item component", () => {
     const checkbox = wrapper.getByRole("checkbox");
 
     expect(checkbox).toBeChecked();
+  });
+
+  it("should activate edit mode when double clicking and summarizing the checkBox Input", () => {
+    const wrapper = render(<TaskItem item={mockTask} />);
+
+    const input = wrapper.getByDisplayValue("Test Task");
+    const checkbox = wrapper.getByRole("checkbox");
+    fireEvent.doubleClick(input);
+
+    expect(checkbox).not.toBeInTheDocument();
   });
 });
